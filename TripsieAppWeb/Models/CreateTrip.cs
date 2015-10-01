31dc147e39880833118cd42d8404568d3548c67e
travismc6 <mcghee.travis@gmail.com>
2015-10-01 10:16:43 -0700

    Create trip cleanup from Labtop

--- TripsieAppWeb/Models/CreateTrip.cs
+++ TripsieAppWeb/Models/CreateTrip.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -11,15 +12,20 @@ namespace TripsieAppWeb.Models
 
         public DateTime End { get; set; }
 
+        [Required]
         public string Destination { get; set; }
 
         public string Description { get; set; }
 
+        [Required]
         public string MyPhone { get; set; }
 
+        [Required]
         public string MyName { get; set; }
 
-        List<Contact> Contacts { get; set; }
+        public string ContactsJson { get; set; }
+
+        //List<Contact> Contacts { get; set; }
          
     }
 }
